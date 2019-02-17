a0b5b2f531d6fc6e6de82eb20c92295fcfbc6c5f
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-17 23:09:22 +0900

    Enable to work ABC116 C

--- AtCoder/ABC116/C.cpp
+++ AtCoder/ABC116/C.cpp
@@ -1,50 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x;
-vector<int> h;
-
-int f(int l, int r)
-{
-	cout << "[" << l << ", " << r << "]" << endl;
-	if (l >= r)
-		return h[l];
-	if (l == r - 1)
-		return max(h[l], h[r]);
-	for (int i = l; i <= r; i++)
-		if (h[i] == 0)
-		{
-			int _l = l, sum = 0;
-			for (int i = l; i <= r; i++)
-			{
-				if (h[i] == 0)
-				{
-					sum += f(_l, i - 1);
-					_l = i;
-				}
-			}
-			return sum;
-		}
-	int _min_;
-	for (int i = l; i <= r; i++)
-	{
-		if (_min_ >= h[i])
-		{
-			_min_ = h[i];
-		}
-	}
-	for (int i = l; i < r; i++)
-		h[i] -= _min_;
-	return f(l, r);
-}
-
+int n, ans, l;
 int main()
 {
 	cin >> n;
+	vector<int> h(n);
 	for (int i = 0; i < n; i++)
 	{
 		cin >> h[i];
 	}
-	cout << f(0, n - 1) << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (l < h[i])
+			ans += h[i] - l;
+		l = h[i];
+	}
+	cout << ans << endl;
 	return 0;
 }
