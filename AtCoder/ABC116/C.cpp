e4b75c38fb0228879334cbd3d076e3f24af0a29e
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-17 17:12:27 +0900

    Change ABC116 C

--- AtCoder/ABC116/C.cpp
+++ AtCoder/ABC116/C.cpp
@@ -4,63 +4,38 @@ using namespace std;
 int n, x;
 vector<int> h;
 
-int times(int begin, int end)
+int f(int l, int r)
 {
-	int _min_, where = begin;
-	if (begin == end - 1)
-	{
-		cout << "sum += " << max(h[begin], h[end]) << endl;
-		return max(h[begin], h[end]);
-	}
-	_min_ = min(h[begin], h[begin + 1]);
-	for (int i = begin; i < end; i++)
+	cout << "[" << l << ", " << r << "]" << endl;
+	if (l >= r)
+		return h[l];
+	if (l == r - 1)
+		return max(h[l], h[r]);
+	for (int i = l; i <= r; i++)
+		if (h[i] == 0)
+		{
+			int _l = l, sum = 0;
+			for (int i = l; i <= r; i++)
+			{
+				if (h[i] == 0)
+				{
+					sum += f(_l, i - 1);
+					_l = i;
+				}
+			}
+			return sum;
+		}
+	int _min_;
+	for (int i = l; i <= r; i++)
 	{
 		if (_min_ >= h[i])
 		{
-			where = i;
 			_min_ = h[i];
 		}
 	}
-	cout << "[" << begin << "," << end << "]" << endl;
-	cout << "min : " << _min_ << endl;
-	cout << "where : " << where << endl;
-	for (int i = begin; i < end; i++)
-		cout << h[i] << ", ";
-	cout << endl;
-	for (int i = begin; i < end; i++)
+	for (int i = l; i < r; i++)
 		h[i] -= _min_;
-	for (int i = begin; i < end; i++)
-		cout << h[i] << ", ";
-	cout << endl;
-
-	int sum = _min_;
-	while (!h[begin])
-	{
-		begin++;
-		if (begin >= end)
-			return _min_;
-	}
-	for (int i = begin; i <= end;)
-	{
-		if (h[i] == 0)
-		{
-			cout << "40L:  [" << begin << "," << i << "]" << endl;
-			sum += times(begin, i);
-			while (!h[begin])
-			{
-				begin++;
-				if (begin >= end)
-					return _min_;
-			}
-			cout << "sum : " << sum << endl;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	cout << "f_sum : " << sum << endl;
-	return sum;
+	return f(l, r);
 }
 
 int main()
@@ -68,41 +43,8 @@ int main()
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> x;
-		h.push_back(x);
+		cin >> h[i];
 	}
-	int sum = 0, begin = 0;
-	while (!h[begin])
-	{
-		begin++;
-		if (begin >= n)
-		{
-			sum += times(begin, n);
-			cout << sum << endl;
-			return 0;
-		}
-	}
-	for (int i = begin; i < n;)
-	{
-		if (h[i] == 0)
-		{
-			cout << "main:  [" << begin << "," << i << "]" << endl;
-			sum += times(begin, i);
-			while (!h[begin])
-			{
-				begin++;
-				if (begin >= n)
-				{
-					cout << sum << endl;
-					return 0;
-				}
-			}
-		}
-		else
-		{
-			i++;
-		}
-	}
-	cout << sum << endl;
+	cout << f(0, n - 1) << endl;
 	return 0;
 }
