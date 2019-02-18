df6ab2e2a1e96061b7675b76f96eca4da9738759
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-18 11:25:34 +0900

    Change ABC001 C

--- AtCoder/ABC001/c.cpp
+++ AtCoder/ABC001/c.cpp
@@ -3,22 +3,15 @@ using namespace std;
 
 int main()
 {
-    int deg, dis;
+    int deg, dis, w;
     double ddis;
     string d[] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
     double beaufort[] = {0.2, 1.5, 3.3, 5.4, 7.9, 10.7, 13.8, 17.1, 20.7, 24.4, 28.4, 32.6};
     cin >> deg >> dis;
-    ddis = floor(((double)dis / 60.0 + 0.05) * 10.0) / 10.0;
-    for (int i = 0; i < 12; i++)
-    {
-        if (ddis <= beaufort[i])
-        {
-            dis = i;
+    ddis = floor(((double)dis / 60.0 + 0.05) * 10.0) / 10.0 - 0.01;
+    for (w = 0; w < 12; w++)
+        if (ddis <= beaufort[w])
             break;
-        }
-        if (i == 11)
-            dis = 12;
-    }
-    cout << ((dis) ? d[(deg * 10 + 1125) / 2250 % 16] : "C") << " " << dis << endl;
+    cout << (w ? d[(deg * 10 + 1125) / 2250 % 16] : "C") << " " << w << endl;
     return 0;
 }
