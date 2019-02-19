a58a6e7f8d45165f014906b8fb8c10a2264c04ee
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-19 20:59:30 +0900

    Tweak ABC002 C

--- AtCoder/ABC002/c.cpp
+++ AtCoder/ABC002/c.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    pair<int, int> a, b, c;
-    cin >> a.first >> a.second >> b.first >> b.second >> c.first >> c.second;
-    b.first -= a.first;
-    c.first -= a.first;
-    b.second -= a.second;
-    c.second -= a.second;
-    cout << fixed << setprecision(2) << (abs((double)(b.first * c.second - b.second * c.first)) / 2.0) << endl;
+    int ax, ay, bx, by, cx, cy;
+    cin >> ax >> ay >> bx >> by >> cx >> cy;
+    bx -= ax;
+    cx -= ax;
+    by -= ay;
+    cy -= ay;
+    cout << fixed << setprecision(2) << (abs((double)(bx * cy - by * cx)) / 2.0) << endl;
     return 0;
 }
