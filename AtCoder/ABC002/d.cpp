e05d3baf91f056b0d6b2da2a83c38b58dfc0f68d
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-19 20:59:12 +0900

    Change ABC002 D

--- AtCoder/ABC002/d.cpp
+++ AtCoder/ABC002/d.cpp
@@ -7,29 +7,35 @@ int main()
     cin >> n >> m;
     vector<int> f(n);
     for (int i = 0; i < m; i++)
+        f[i] |= 1 << i;
+    for (int i = 0; i < m; i++)
     {
         cin >> x >> y;
-        f[x] |= 1 << (y - 1);
-        f[y] |= 1 << (x - 1);
+        f[x - 1] |= 1 << (y - 1);
+        f[y - 1] |= 1 << (x - 1);
     }
-    for (int i = 0; i < n; i++)
-        std::cout << std::bitset<12>(f[i]) << endl;
 
-    
-    for (int i = 0; i < 2 << n; i++)
+    //for (int i = 0; i < n; i++)
+    //    std::cout << std::bitset<12>(f[i]) << endl;
+    //cout << endl;
+
+    //cout << n << " " << (1 << n) << endl;
+    for (int i = 0; i < 1 << n; i++)
     {
         int sum = i;
+        if (ans > bitset<32>(i).count())
+            continue;
+        // cout << i << ": ";
         for (int j = 0; j < n; j++)
         {
-            if (f[i] & 1 << j)
+            if (i >> j & 1)
             {
-                std::cout << "-> " << std::bitset<12>(f[i] & 1 << j) << endl;
-                sum &= f[i];
+                sum &= f[j];
             }
         }
         if (ans < bitset<32>(sum).count())
             ans = bitset<32>(sum).count();
-        if (sum) std::cout << ":) " << std::bitset<12>(sum) << endl;
+        // std::cout << std::bitset<12>(sum) << endl;
     }
     cout << ans << endl;
     return 0;
