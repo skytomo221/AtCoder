f0346b8b02eef3bec53dfe04632320897f106f91
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-19 23:54:42 +0900

    Enable to work ABC002 D

--- AtCoder/ABC002/d.cpp
+++ AtCoder/ABC002/d.cpp
@@ -6,27 +6,22 @@ int main()
     int n, m, x, y, ans = 0;
     cin >> n >> m;
     vector<int> f(n);
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < n; i++)
         f[i] |= 1 << i;
     for (int i = 0; i < m; i++)
     {
         cin >> x >> y;
-        f[x - 1] |= 1 << (y - 1);
-        f[y - 1] |= 1 << (x - 1);
+        x--;
+        y--;
+        f[x] |= 1 << y;
+        f[y] |= 1 << x;
     }
-
-    //for (int i = 0; i < n; i++)
-    //    std::cout << std::bitset<12>(f[i]) << endl;
-    //cout << endl;
-
-    //cout << n << " " << (1 << n) << endl;
     for (int i = 0; i < 1 << n; i++)
     {
         int sum = i;
         if (ans > bitset<32>(i).count())
             continue;
-        // cout << i << ": ";
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j < n; j++)
         {
             if (i >> j & 1)
             {
@@ -35,7 +30,6 @@ int main()
         }
         if (ans < bitset<32>(sum).count())
             ans = bitset<32>(sum).count();
-        // std::cout << std::bitset<12>(sum) << endl;
     }
     cout << ans << endl;
     return 0;
