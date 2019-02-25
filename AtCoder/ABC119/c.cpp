5add9e48911138999ebe6cd02bc88ed1c22555a8
skytomo <tomo1kawa.fullerene@gmail.com>
2019-02-25 20:43:23 +0900

    Enable to work ABC119 C

--- AtCoder/ABC119/c.cpp
+++ AtCoder/ABC119/c.cpp
@@ -1,65 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, a, b, c;
+vector<int> l;
+
+int dfs(int cur, int ma, int mb, int mc)
+{
+    if (cur == n)
+        return (min({ma, mb, mc}) > 0)
+                   ? abs(a - ma) + abs(b - mb) + abs(c - mc) - 30
+                   : 1e9;
+    int ret0 = dfs(cur + 1, ma, mb, mc);
+    int ret1 = dfs(cur + 1, ma + l[cur], mb, mc) + 10;
+    int ret2 = dfs(cur + 1, ma, mb + l[cur], mc) + 10;
+    int ret3 = dfs(cur + 1, ma, mb, mc + l[cur]) + 10;
+    return min({ret0, ret1, ret2, ret3});
+}
+
 int main()
 {
-    int n, ans = 0;
-    vector<int> a(3), a_min(3);
-    cin >> n >> a[0] >> a[1] >> a[2];
-    vector<int> l(n);
+    cin >> n >> a >> b >> c;
+    l.resize(n);
     for (int i = 0; i < n; i++)
-    {
         cin >> l[i];
-        a_min[i] = 1001;
-    }
-    sort(a.begin(), a.end());
-    sort(l.begin(), l.end());
-    int _min_ = 1001;
-    vector<int> min_l;
-    for (int i = 0; i < 1 << l.size(); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < l.size(); j++)
-        {
-            if (i >> j & 1)
-            {
-                sum += l[j];
-            }
-        }
-        cout << bitset<8>(i) << " " << sum << endl;
-        if (_min_ > abs(sum - (a[0] + a[1] + a[2])))
-        {
-            _min_ = abs(sum - (a[0] + a[1] + a[2]));
-            min_l.clear();
-            for (int j = 0; j < l.size(); j++)
-            {
-                if (i >> j & 1)
-                {
-                    min_l.push_back(l[j]);
-                }
-            }
-        }
-    }
-    cout << _min_ << endl;
-    /*    for (int i = 0; i < a.size(); i++)
-    {
-        int _min_ = 1001;
-        auto r = find(l.begin(), l.end(), a[i]);
-        if (r != l.end())
-        {
-            l.erase(r);
-            continue;
-        }
-        else
-        {
-            for (int j = 0; j < n; j++)
-            {
-                _min_ = min(_min_, abs(a[i] - l[j]));
-            }
-        }
-        ans += _min_;
-    }
-*/
-    cout << ans << endl;
+    cout << dfs(0, 0, 0, 0) << endl;
     return 0;
 }
