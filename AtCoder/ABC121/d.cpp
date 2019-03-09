1709469e3d715d71fc0993744b5758ce075891c8
skytomo <tomo1kawa.fullerene@gmail.com>
2019-03-10 01:21:35 +0900

    Enable to work ABC121D

--- AtCoder/ABC121/d.cpp
+++ AtCoder/ABC121/d.cpp
@@ -1,24 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long int count(long long int end, long long int mod)
-{
-    return end % mod + end / mod;
-}
-
 int main()
 {
-    long long int a, b, ans = 0;
+    long long int a, b;
     cin >> a >> b;
-    for (long long int i = 0; i < 32; i++)
-    {
-        long long int cnt = count(b, 1LL << (i + 1)) - count(a, 1LL << (i + 1));
-        cout << (1LL << i) << " " << cnt << endl;
-        if (cnt % 2 != 0)
-        {
-            ans += 1 << i;
-        }
-    }
+    long long int begin = a % 2 ? a + 1 : a, end = b % 2 ? b : b - 1;
+    long long int ans = (a % 2 ? a : 0) ^ ((end - begin + 1) / 2 % 2) ^ (b % 2 ? 0 : b);
     cout << ans << endl;
     return 0;
 }
